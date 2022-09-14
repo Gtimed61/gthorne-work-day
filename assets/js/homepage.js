8a698323a1f7ca0bd0d489b5d8e74d5756141d5f
Thorne <gthorne61@gmail.com>
2022-09-13 21:37:16 -0500

    time variables

---
+++ assets/js/homepage.js
@@ -0,0 +1,23 @@
+//daily planner
+//current day displayed on open
+var currentDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+document.getElementById("today").textContent = currentDate;
+//time variable
+var time9 = moment().hour(9);
+var time10 = moment().hour(10);
+var time11 = moment().hour(11);
+var time12 = moment().hour(12);
+var time1 = moment().hour(13);
+var time2 = moment().hour(14);
+var time3 = moment().hour(15);
+var time4 = moment().hour(16);
+var time5 = moment().hour(17);
+//scroll down to view time blocks  
+//time blocks are coded to change color for past, present, future
+//able to enter event in each time block
+//save time block
+//save text in local storage
+var saveText = function() {
+    localStorage.setItem("text", JSON.stringify(text));
+}
+//on refresh, saved events persist 
