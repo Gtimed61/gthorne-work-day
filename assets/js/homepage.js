eabe73f3207f7b85463ee8fc5c6fe200954d5f25
Thorne <gthorne61@gmail.com>
2022-09-14 22:18:21 -0500

    button setup

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,12 +12,20 @@ var time2 = moment().hour(14);
 var time3 = moment().hour(15);
 var time4 = moment().hour(16);
 var time5 = moment().hour(17);
-//scroll down to view time blocks  
+//scroll down to view time blocks
+var hourBlock = [time9, time10, time11, time12, time13, time14, 
 //time blocks are coded to change color for past, present, future
 //able to enter event in each time block
+$( ".selector" ).selectable({
+    selected: function( event, ui ) {}
+  });
 //save time block
+
 //save text in local storage
 var saveText = function() {
     localStorage.setItem("text", JSON.stringify(text));
 }
+
+$( ".selector" ).on( "selectableselected", function( event, ui ) {} );
+
 //on refresh, saved events persist 
